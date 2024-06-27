26eed867037c56355400a8309129b6a771015bf2
Lucas Faudman <Lucasfaudman@gmail.com>
2024-06-27 01:24:40 -0700

    upload debug script to repo

---
+++ debug.py
@@ -0,0 +1,17 @@
+from code import interact
+from json import load
+from src.linkedin_ai import liautomator, app, models
+
+# app.main()
+
+with open("linkedin-ai-config.json") as f:
+    config = load(f)
+    config.pop("li_auto_login", None)
+
+li_auto = liautomator.LinkedInAutomator(**config)
+li_auto.init_dbs()
+li_auto.init_scraper()
+li_auto.login()
+
+jobs = li_auto.job_app_db.get_models(models.Job, *input("Input test job ids: ").strip().split())
+interact(local=locals())
