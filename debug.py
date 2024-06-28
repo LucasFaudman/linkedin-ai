492d502304714f88acbf2e8d7a83431ef4e2835a
Lucas Faudman <Lucasfaudman@gmail.com>
2024-06-28 11:16:23 -0700

    debug choice of front or backend

--- debug.py
+++ debug.py
@@ -2,14 +2,17 @@ from code import interact
 from json import load
 from src.linkedin_ai import liautomator, app, models
 
-with open("linkedin-ai-config.json") as f:
-    config = load(f)
-    config.pop("li_auto_login", None)
-
-li_auto = liautomator.LinkedInAutomator(**config)
-li_auto.init_dbs()
-li_auto.init_scraper()
-li_auto.login()
-
-jobs = li_auto.job_app_db.get_models(models.Job, *input("Input test job ids: ").strip().split())
-interact(local=locals())
+if __name__ == "__main__":
+    if (choice := input("Debug app (a)? linkedin-ai (l)? quit (q)?").strip().lower()).startswith("a"):
+        app.main()
+    elif choice.startswith("l"):
+        with open("linkedin-ai-config.json") as f:
+            config = load(f)
+            config.pop("li_auto_login", None)
+        li_auto = liautomator.LinkedInAutomator(**config)
+        li_auto.init_dbs()
+        li_auto.init_scraper()
+        li_auto.login()
+        interact(local=locals())
+    else:
+        exit()
